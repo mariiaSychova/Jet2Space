dd12a3fcd3f80b1043cb3363e83f2e5e925800a5
Oleksandra Malii <minelenova1@gmail.com>
2025-11-01 21:44:47 +0200

    feat: add js file to export animations to web

---
+++ client/src/data/planets.js
@@ -0,0 +1,47 @@
+export const planets = [
+  {
+    id: 'sun',
+    name: 'Сонце',
+    animationPath: '/animations/sun.webm',
+  },
+  {
+    id: 'mercury',
+    name: 'Меркурій',
+    animationPath: '/animations/mercury.webm',
+  },
+  {
+    id: 'venus',
+    name: 'Венера',
+    animationPath: '/animations/venus.webm',
+  },
+  {
+    id: 'earth',
+    name: 'Земля',
+    animationPath: '/animations/earth.webm',
+  },
+  {
+    id: 'mars',
+    name: 'Марс',
+    animationPath: '/animations/mars.webm',
+  },
+  {
+    id: 'jupiter',
+    name: 'Юпітер',
+    animationPath: '/animations/jupiter.webm',
+  },
+  {
+    id: 'saturn',
+    name: 'Сатурн',
+    animationPath: '/animations/saturn.webm',
+  },
+  {
+    id: 'uranus',
+    name: 'Уран',
+    animationPath: '/animations/uranus.webm',
+  },
+  {
+    id: 'neptune',
+    name: 'Нептун',
+    animationPath: '/animations/neptune.webm',
+  },
+];
