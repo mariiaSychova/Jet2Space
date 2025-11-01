c797f527243043d081b439478b8685bdb3dc8c4b
Oleksandra Malii <minelenova1@gmail.com>
2025-11-02 00:02:55 +0200

    feat: add dynamic sizing to planets

--- client/src/components/Planet.vue
+++ client/src/components/Planet.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="planet-wrapper" @click="goToPlanetDetail" @mouseover="playVideo" @mouseleave="pauseVideo">
-        <video ref="videoPlayer" :src="planet.animationPath" class="planet-video" :autoplay="false" :loop="true" muted
+        <video ref="videoPlayer" :style="planetStyle" :src="planet.animationPath" class="planet-video" :autoplay="false" :loop="true" muted
             playsinline></video>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const props = defineProps({
@@ -18,6 +18,14 @@ const props = defineProps({
 
 const router = useRouter()
 const videoPlayer = ref(null)
+
+const planetStyle = computed(() => {
+  const size = props.planet.size || 200
+  return {
+    width: `${size}px`,
+    height: `${size}px`,
+  }
+})
 
 function playVideo() {
     if (videoPlayer.value) {
@@ -38,9 +46,6 @@ function goToPlanetDetail() {
 
 <style scoped>
 .planet-wrapper {
-  /* Встановлюємо розмір та форму */
-  width: 200px;
-  height: 200px; 
   border-radius: 50%;
   
   display: flex;
@@ -64,8 +69,10 @@ function goToPlanetDetail() {
   pointer-events: none;
   
   transition: transform 0.4s ease;
+
+  transform: scale(1.08);
 }
 .planet-wrapper:hover .planet-video {
-  transform: scale(1.1); 
+    transform: scale(1.188); 
 }
 </style>
